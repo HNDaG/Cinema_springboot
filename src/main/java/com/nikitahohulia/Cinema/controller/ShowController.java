5dc951a509e258f0fd5234ad35a0858461677ddf
NikitaHohulia <nikitahohulia@gmail.com>
2023-06-06 12:51:43 +0300

    Init

---
+++ src/main/java/com/nikitahohulia/Cinema/controller/ShowController.java
@@ -0,0 +1,31 @@
+package com.nikitahohulia.Cinema.controller;
+
+import com.nikitahohulia.Cinema.services.MovieService;
+import com.nikitahohulia.Cinema.services.ScheduleService;
+import com.nikitahohulia.Cinema.wrappers.ShowWrapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@Controller
+@AllArgsConstructor
+public class ShowController {
+
+    private MovieService movieService;
+    private ScheduleService scheduleService;
+
+    @GetMapping("/show/{movieId}")
+    public String showSchedule(@PathVariable("movieId") Integer id, Model theModel) {
+
+        ShowWrapper showWrapper = movieService.getShowPage(id);
+        theModel.addAttribute("showWrapper", showWrapper);
+        return "shows";
+
+    }
+
+}
+
